ba7881447244e45965524d50ed70f5d9b65ee36a
Yogeshcrown <yogeshsharmatss@gmail.com>
2024-05-23 15:42:06 +0530

    I hve made the negative test

--- cypress/e2e/My First Test.cy.js
+++ cypress/e2e/My First Test.cy.js
@@ -1,6 +1,6 @@
 describe('My First Test', () => {
   it('Verify the Title of the Website', () => {
     cy.visit('https://www.crownstack.com/')
-    cy.title().should('eq','Crownstack Technologies')
+    cy.title().should('eq','Crownstack Technolos')
   })
 })
